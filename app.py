5bc1c1729a04a62c1e682e433ab190b92eb14c34
sandrika9 <solomnishvilisandro@gmail.com>
2026-08-29 14:17:31 +0400

    Update app.py

--- app.py
+++ app.py
@@ -16,15 +16,15 @@ app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = "your_email@gmail.com"  # შენი მეილი
-app.config["MAIL_PASSWORD"] = "your_app_password"     # აპლიკაციის პაროლი
+app.config["MAIL_PASSWORD"] = "your_app_password"      # აპლიკაციის პაროლი
 app.config["MAIL_DEFAULT_SENDER"] = "your_email@gmail.com"
 
 mail = Mail(app)
 
-# Supabase PostgreSQL ბაზის მისამართი
+# Supabase PostgreSQL ბაზის მისამართი (სწორი კოდირებით)
 DATABASE_URL = os.environ.get(
     "DATABASE_URL", 
-    "postgresql://postgres.rnktcgfknokfdktfxjkb:Sandrika789%24@aws-0-ap-northeast-2.pooler.supabase.com:6543/postgres"
+    "postgresql://postgres.rnktcgfknokfdktfxjkb:Sandrika789%24%40aws-0-ap-northeast-2.pooler.supabase.com:6543/postgres"
 )
 
 
